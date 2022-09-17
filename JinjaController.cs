46e7640ff0fe0d402c96dab20bcfc6ede3abd2b6
Tim Sun <73847178+timsssun@users.noreply.github.com>
2022-09-17 10:37:15 -0400

    Adding left arrow

--- JinjaController.cs
+++ JinjaController.cs
@@ -11,9 +11,16 @@ public class JinjaController : MonoBehaviour {
 	// Update is called once per frame
 	void Update() {
 
+		Transform tf = GetComponent<Transform>();
+
 		//check if the arrow key is down, move jinja
 		if (Input.GetKey(KeyCode.RightArrow)) {
-			GetComponent<Transform>().position += Vector3.right;
+			tf.position += Vector3.right * 0.1f;
+		}
+
+		//check if left arrow key is down, move jinja left
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			tf.position += Vector3.left * 0.1f;
 		}
 	}
 }
