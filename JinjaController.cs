59bc4e334090ec17250628ac5bea3f2a8a5ad57a
Tim Sun <73847178+timsssun@users.noreply.github.com>
2022-09-17 10:24:06 -0400

    Jinja listening to right arrow

--- JinjaController.cs
+++ JinjaController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class JinjaController : MonoBehaviour {
-
 	// Start is called before the first frame update
 	void Start() {
 
@@ -12,6 +11,9 @@ public class JinjaController : MonoBehaviour {
 	// Update is called once per frame
 	void Update() {
 
+		//check if the arrow key is down, move jinja
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			GetComponent<Transform>().position += Vector3.right;
+		}
 	}
-
 }
